7a6fcbf22341b6fc99a459e1c1bf982939a1e213
Michael Mayer <mjmayer@ucdavis.edu>
2023-07-03 11:27:58 -0700

    Add additional thumbprint
    There are multiple intermediary certificats for the Actions SSL. This
    change adds the second thumbrint.
    
    See
    https://github.com/aws-actions/configure-aws-credentials/commit/0270d0bcecaf2c76c8fbf7bf3de0d65a6d06e076
    for additional information.

--- variables.tf
+++ variables.tf
@@ -19,7 +19,8 @@ variable "client_id_list" {
 variable "thumbprint_list" {
   type = list(string)
   default = [
-    "6938fd4d98bab03faadb97b34396831e3780aea1"
+    "6938fd4d98bab03faadb97b34396831e3780aea1",
+    "1c58a3a8518e8759bf075b76b750d4f2df264fcd"
   ]
 }
 
